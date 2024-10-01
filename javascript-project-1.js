3e964081a462c98bd46f41115f49d35ffb397c84
Sheladiya-shubham <159759051+Sheladiya-shubham@users.noreply.github.com>
2024-10-01 15:26:33 +0530

    Add files via upload

---
+++ javascript-project-1.js
@@ -0,0 +1,96 @@
+// ### 1. **Basic Addition**
+// ```js
+// What is the output of the following code?
+
+let a = 10;
+let b = 20;
+
+console.log(a + b);
+
+// ### 2. **Subtraction**
+// ```js
+// What is the result of the following operation?
+
+let x = 50;
+let y = 25;
+
+console.log(x-y);
+
+// ### 3. **Multiplication**
+// ```js
+// What will be printed to the console?
+
+let m = 6;
+let n = 7;
+
+console.log(m*n);
+
+
+// ### 4. **Division**
+// ```js
+// What is the output of the following code?
+
+let p = 20;
+let q = 4;
+
+console.log(p/q);
+
+// ### 5. **Modulo (Remainder)**
+// ```js
+// What does the following code return?
+
+let num1 = 15;
+let num2 = 4;
+
+console.log(num1 % num2);
+
+
+
+// ### 6. **Exponentiation (Power)**
+// ```js
+// What will this code return?
+
+let base = 3;
+let exponent = 4;
+
+console.log(base**exponent);
+
+
+
+// ### 7. **Comparison (Greater Than)**
+// ```js
+// What will the following comparison return?
+
+let l = 10;
+let k = 15;
+
+
+console.log(l>k);
+
+
+// ### 8. **Comparison (Less Than or Equal)**
+// ```js
+// What will be the result of this comparison?
+
+let s = 25;
+let t = 25;
+
+
+console.log(s<=t);
+
+
+
+// ### 9. **Equality Check**
+// ```js
+// What is the output of this equality check?
+
+let h = 5;
+console.log(h == '5');
+
+
+// ### 10. **Strict Equality Check**
+// ```js
+// What will this strict equality check return?
+
+let num = 5;
+console.log(num === '5');
